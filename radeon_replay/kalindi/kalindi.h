7e2e55aeed512bbed6a83604d4e9e086580c61b0
Damien Zammit <damien@zamaudio.com>
2018-05-16 17:55:02 +1000

    Clone Aruba for Kalindi HD8400 (Kabini APU eg Biostar A68N-5200)
    - Obviously don't run this on the hardware, it needs to be rewritten.
      This is just a pure clone of the aruba directory with s/aruba/kalindi
      to prepare for porting to this hardware, (plus cikd.h from Linux).
    
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

---
+++ radeon_replay/kalindi/kalindi.h
@@ -0,0 +1,123 @@
+#ifndef __KALINDI_H
+#define __KALINDI_H
+
+#include "linux_glue.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+
+enum pll_ids {
+	PPLL_1 = 0,
+	PPLL_2 = 1,
+	PPLL_0 = 2,
+	DCPLL = PPLL_0,
+};
+
+enum encoder_phy {
+	ENCODER_UNIPHY_A,
+	ENCODER_UNIPHY_B,
+	ENCODER_UNIPHY_C,
+	ENCODER_UNIPHY_D,
+	ENCODER_UNIPHY_E,
+	ENCODER_UNIPHY_F,
+	ENCODER_UNIPHY_G,
+};
+
+enum transmitter_mode {
+	TRANSMITTER_MODE_DP,
+	TRANSMITTER_MODE_LVDS,
+	TRANSMITTER_MODE_DVI,
+	TRANSMITTER_MODE_HDMI,
+	TRANSMITTER_MODE_DP_MST,
+};
+
+enum XMIT_FLAGS {
+	XMIT_FLAG_BLACKMAGIC = (1 << 0),
+	XMIT_FLAG_COHERENT = (1 << 1),
+};
+
+struct pll_settings {
+	uint32_t fb_div_frac;
+	uint16_t fb_div_int;
+	uint8_t ref_div;
+	uint8_t post_div;
+};
+
+/* backlight.c */
+void kalindi_backlight_init(struct radeon_device *rdev, bool controls_edp);
+void kalindi_backlight_control(struct radeon_device *rdev,
+			     uint16_t bl_pwm_freq_hz, uint8_t bl_level);
+void kalindi_backlight_blon(struct radeon_device *rdev);
+void kalindi_backlight_bloff(struct radeon_device *rdev);
+
+/* crtc.c */
+int kalindi_enable_crtc(struct radeon_device *rdev, uint8_t crtc_id, bool enable);
+int kalindi_update_crtc_x2_buf(struct radeon_device *rdev,
+			     uint8_t crtc_id, bool enable);
+int kalindi_blank_crtc(struct radeon_device *rdev, uint8_t crtc_id, bool enable);
+void kalindi_set_crtc_dtd_timing(struct radeon_device *rdev, uint8_t crtc_id,
+			       struct drm_display_mode *mode);
+void kalindi_overscan_setup(struct radeon_device *rdev, uint8_t crtc_id,
+			  uint8_t h_border, uint8_t v_border);
+
+/* dp_aux.c */
+int radeon_read_dpcd(uint8_t bus, uint8_t *dest, uint16_t start, uint16_t len);
+int radeon_read_dp_aux_i2c(uint8_t bus, uint8_t addr,
+			   uint8_t *dest, uint8_t start, uint16_t len);
+
+/* encoder.c */
+void kalindi_encoder_setup_dp(struct radeon_device *rdev, uint8_t id,
+			    uint16_t pixel_clock_khz, uint8_t lane_num,
+			    uint8_t bpc, uint32_t dp_link_rate);
+void kalindi_encoder_setup_other(struct radeon_device *rdev, uint8_t id,
+			       uint8_t mode, uint8_t lane_num);
+void kalindi_encoder_setup_panel_mode(struct radeon_device *rdev,
+				    uint8_t encoder_id, uint8_t panel_mode);
+void kalindi_encoder_video_on(struct radeon_device *rdev, uint8_t encoder_id);
+void kalindi_encoder_video_off(struct radeon_device *rdev, uint8_t encoder_id);
+void kalindi_encoder_link_training_start(struct radeon_device *rdev,
+				       uint8_t encoder_id);
+void kalindi_encoder_link_training_pattern(struct radeon_device *rdev,
+					 uint8_t encoder_id, uint8_t pattern);
+void kalindi_encoder_link_training_finish(struct radeon_device *rdev,
+					uint8_t encoder_id);
+void kalindi_set_encoder_crtc_source(struct radeon_device *rdev,
+				   uint8_t crtc_id, uint8_t encoder_id,
+				   uint8_t encoder_mode);
+
+/* init_misc.c */
+void dynamic_clock_gating_init_hp_1035dx(struct radeon_device *rdev);
+
+/* misc.c */
+uint32_t kalindi_get_block_offest(uint8_t block_id);
+void kalindi_powergate_crtc(struct radeon_device *rdev, uint8_t crtc_id,
+			  bool enable);
+void kalindi_scaler_setup(struct radeon_device *rdev, uint8_t crtc_id,
+			enum radeon_rmx_type rmx_type);
+
+/* pll.c */
+uint8_t kalindi_compute_engine_pll(uint32_t vco_freq_mhz, uint32_t *clock_khz);
+void kalindi_disable_pll_ss(struct radeon_device *rdev, uint8_t pll_id);
+void kalindi_enable_pll_ss(struct radeon_device *rdev, uint8_t pll_id,
+			 uint16_t step, uint16_t ds_frac, uint8_t fbdiv,
+			 uint8_t nfrac, bool downspread);
+void kalindi_set_pixel_clock(struct radeon_device *rdev, enum pll_ids pll_id,
+			   struct pll_settings *pll_divs, uint8_t crtc_id,
+			   uint32_t px_clk, uint32_t fags, uint8_t encoder_mode);
+void kalindi_init_pixel_pll(struct radeon_device * rdev, uint8_t pll_id);
+int kalindi_set_disp_eng_pll(struct radeon_device *rdev, uint32_t vco_freq_mhz,
+			   uint32_t clock_khz, uint32_t default_clock_khz,
+			   uint16_t pcie_ss_percent);
+
+/* transmitter.c */
+void kalindi_transmitter_enable(struct radeon_device *rdev, enum encoder_phy ephy,
+			      uint8_t num_lanes, uint32_t symbol_clk,
+			      enum pll_ids phy_src_pll,
+			      enum transmitter_mode phy_mode,
+			      uint8_t src_encoder_id,
+			      uint8_t hpd_id,
+			      uint32_t xmit_flags);
+void kalindi_transmitter_vsemph(struct radeon_device *rdev, uint8_t phy_id,
+			      uint8_t lane_sel);
+
+#endif	/* __KALINDI_H */
